3c007065b43de5514e8d66af2b75fbe71e121b7a
Bufutda <saw@tzky.ca>
2016-11-12 16:10:27 -0700

    File normalization

--- Assignment 3/FastFtp.java
+++ Assignment 3/FastFtp.java
@@ -5,26 +5,26 @@ import cpsc441.a3.*;
 
 /**
  * FastFtp Class
- * 
+ *
  * FastFtp implements a basic FTP application based on UDP data transmission.
- * The main mehtod is send() which takes a file name as input argument and send the file 
+ * The main mehtod is send() which takes a file name as input argument and send the file
  * to the specified destination host.
- * 
+ *
  */
 public class FastFtp {
-	
+
     /**
-     * Constructor to initialize the program 
-     * 
+     * Constructor to initialize the program
+     *
      * @param windowSize	Size of the window for Go-Back_N (in segments)
      * @param rtoTimer		The time-out interval for the retransmission timer (in milli-seconds)
      */
-	public FastFtp(int windowSize, int rtoTimer) {
-		//
-		// to be completed
-		//
-	}
-	
+    public FastFtp(int windowSize, int rtoTimer) {
+        //
+        // to be completed
+        //
+    }
+
 
     /**
      * Sends the specified file to the specified destination host:
@@ -32,54 +32,52 @@ public class FastFtp {
      * 2. send file segment by segment over UDP
      * 3. send end of transmission over tcp
      * 3. clean up
-     * 
+     *
      * @param serverName	Name of the remote server
      * @param serverPort	Port number of the remote server
      * @param fileName		Name of the file to be trasferred to the rmeote server
      */
-	public void send(String serverName, int serverPort, String fileName) {
-		//
-		// to be completed
-		//
-	}
+    public void send(String serverName, int serverPort, String fileName) {
+        //
+        // to be completed
+        //
+    }
 
-	
+
     /**
      * A simple test driver
-     * 
+     *
      */
-	public static void main(String[] args) {
-		int windowSize = 10; //segments
-		int timeout = 100; // milli-seconds
-		
-		String serverName = "localhost";
-		String fileName = "";
-		int serverPort = 0;
-		
-		// check for command line arguments
-		if (args.length == 3) {
-			// either privide 3 paramaters
-			serverName = args[0];
-			serverPort = Integer.parseInt(args[1]);
-			fileName = args[2];
-		}
-		else if (args.length == 2) {
-			// or just server port and file name
-			serverPort = Integer.parseInt(args[0]);
-			fileName = args[1];
-		}
-		else {
-			System.out.println("wrong number of arguments, try agaon.");
-			System.out.println("usage: java FastFtp server port file");
-			System.exit(0);
-		}
+    public static void main(String[] args) {
+        int windowSize = 10; //segments
+        int timeout = 100; // milli-seconds
 
-		
-		FastFtp ftp = new FastFtp(windowSize, timeout);
-		
-		System.out.printf("sending file \'%s\' to server...\n", fileName);
-		ftp.send(serverName, serverPort, fileName);
-		System.out.println("file transfer completed.");
-	}
+        String serverName = "localhost";
+        String fileName = "";
+        int serverPort = 0;
+
+        // check for command line arguments
+        if (args.length == 3) {
+            // either privide 3 paramaters
+            serverName = args[0];
+            serverPort = Integer.parseInt(args[1]);
+            fileName = args[2];
+        } else if (args.length == 2) {
+            // or just server port and file name
+            serverPort = Integer.parseInt(args[0]);
+            fileName = args[1];
+        } else {
+            System.out.println("wrong number of arguments, try agaon.");
+            System.out.println("usage: java FastFtp server port file");
+            System.exit(0);
+        }
+
+
+        FastFtp ftp = new FastFtp(windowSize, timeout);
+
+        System.out.printf("sending file \'%s\' to server...\n", fileName);
+        ftp.send(serverName, serverPort, fileName);
+        System.out.println("file transfer completed.");
+    }
 
 }
